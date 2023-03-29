ad31c230d0d87bf9e8017aca332e8c9b33b21ef3
mikkanen1 <91195560+mikkanen1@users.noreply.github.com>
2023-03-29 21:59:19 +0300

    Update index.js

--- guestbook/data/index.js
+++ guestbook/data/index.js
@@ -1,5 +1,3 @@
-const { MongoClient } = require('mongodb');
-
 const express = require('express');
 const app = express();
 const port = 3000;
@@ -15,45 +13,36 @@ app.post('/add', (req, res) => {
   const name = req.body.name;
   const message = req.body.message;
 
-  const fs = require('fs');
-const dataPath = './data.json';
+  MongoClient.connect('mongodb://localhost:27017', { useUnifiedTopology: true }, (err, client) => {
+  if (err) {
+    console.error(err);
+    res.sendStatus(500);
+    return;
+  }
 
-fs.readFile(dataPath, (err, data) => {
-  if (err) throw err;
-  const guestbookEntries = JSON.parse(data);
-  const newEntry = { name: name, message: message };
-  guestbookEntries.push(newEntry);
-  fs.writeFile(dataPath, JSON.stringify(guestbookEntries), (err) => {
-    if (err) throw err;
+  const db = client.db('guestbook');
+
+  db.collection('entries').insertOne({
+    name: name,
+    message: message,
+    timestamp: new Date()
+  }, (err, result) => {
+    if (err) {
+      console.error(err);
+      res.sendStatus(500);
+      return;
+    }
+
+    console.log('Saved entry to MongoDB:', result.ops[0]);
     res.redirect('/');
   });
 });
 
+  // TODO: save name and message to a database or file
 
   res.redirect('/');
 });
 
 app.listen(port, () => {
   console.log(`Guestbook app listening at http://localhost:${port}`);
-});
-
-const fs = require('fs');
-const path = require('path');
-
-// ...
-
-app.post('/add', (req, res) => {
-  const name = req.body.name;
-  const message = req.body.message;
-  const filePath = path.join(__dirname, 'entries.txt');
-  const entry = `${name}: ${message}\n`;
-
-  fs.appendFile(filePath, entry, (err) => {
-    if (err) {
-      console.error(err);
-      res.status(500).send('Error saving guestbook entry');
-    } else {
-      res.redirect('/');
-    }
-  });
 });
