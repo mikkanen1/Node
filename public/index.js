df06fb54f363b56bb9fed0239cda3c8439292fa8
mikkanen1 <91195560+mikkanen1@users.noreply.github.com>
2023-05-24 16:15:41 +0300

    A

--- public/index.js
+++ public/index.js
@@ -9,7 +9,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
 
 app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/guestbook.html');
+  res.sendFile(__dirname + '\\guestbook.html');
 });
 
 app.post('/add', (req, res) => {
