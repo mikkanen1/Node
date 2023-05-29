b00245c9bc8d9b3b8c3e0932eb9accabd4143e18
mikkanen1 <91195560+mikkanen1@users.noreply.github.com>
2023-05-29 13:35:13 +0300

    A

--- nextjs-blog/pages/posts/first-post.js
+++ nextjs-blog/pages/posts/first-post.js
@@ -1,10 +1,11 @@
 import Script from 'next/script';
 import Head from 'next/head';
 import Link from 'next/link';
+import Layout from '../../components/layout';
 
 export default function FirstPost() {
   return (
-    <>
+    <Layout>
       <Head>
         <title>First Post</title>
         <script src="https://connect.facebook.net/en_US/sdk.js" />
@@ -20,6 +21,6 @@ export default function FirstPost() {
       <h2>
         <Link href="/">← Back to home</Link>
       </h2>
-    </>
+      </Layout>
   );
 }
