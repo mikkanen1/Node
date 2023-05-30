704b061a03b03d9cc8c6828ac71922597c146bdf
mikkanen1 <91195560+mikkanen1@users.noreply.github.com>
2023-05-30 17:06:13 +0300

    Update index.js

--- nextjs-blog/pages/index.js
+++ nextjs-blog/pages/index.js
@@ -9,7 +9,7 @@ export default function Home() {
         <title>{siteTitle}</title>
       </Head>
       <section className={utilStyles.headingMd}>
-        <p>I used to be an african warlord until I got desperate enough to go to UK to safety, my personal advise: Don´t do that I somehow get stabbed more here than in Kongo</p>
+        <p>I used to be an african warlord until I got desperate enough to go to UK to safety, my personal advise: Don´t do that, I somehow get stabbed more here than in Kongo</p>
         <p>
           (This is a sample website - you’ll be building a site like this on{' '}
           <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
